59940450bf709681f7c56610c7e44f5e2fc1eb40
Scheila Stihler <sstihler@gmail.com>
2024-07-27 18:22:02 -0300

    feat: estrutura da página cadastro de notas

--- src/app/pages/cadastro-notas/cadastro-notas.component.ts
+++ src/app/pages/cadastro-notas/cadastro-notas.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-notas',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './cadastro-notas.component.html',
-  styleUrl: './cadastro-notas.component.scss'
+  styleUrl: './cadastro-notas.component.scss',
 })
 export class CadastroNotasComponent {
+  formNota!: FormGroup;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.formNota = new FormGroup({
+      professor: new FormControl('', Validators.required),
+      materia: new FormControl('', Validators.required),
+      avaliacao: new FormControl('', Validators.required),
+      data: new FormControl('', Validators.required),
+      aluno: new FormControl('', Validators.required),
+      nota: new FormControl('', Validators.required),
+    });
+  }
+
+  submitForm() {
+    window.alert('Dados gravados com sucesso!');
+  }
+
+  cancelar() {
+    this.formNota.reset();
+    this.router.navigate(['/home']);
+  }
 }
